52270fc177136558cca4507ea1861eeb6c3eaf14
[AndreaS] <solfa_90@hotmail.it>
2021-08-20 11:05:03 +0200

    sort and sorted

--- index.py
+++ index.py
@@ -401,7 +401,6 @@ for val in range(2):
 for name in names:
     print(f'{name.lower().title()}! {msg}')
 
-"""
 # Enumerate This!
 friends = ['Brian', 'Judith', 'Reg', 'Loretta', 'Colin']
 
@@ -410,3 +409,27 @@ for num, friend in enumerate(friends, 1):
 
 for friend in enumerate(enumerate(friends, 50), -100):
     print(friend)
+"""
+# Sort - Sorted
+my_list = [1, 5, -3, 7, -2]
+my_dict = {'car': 4, 'dog': 2, 'add': 3, 'bee': 1}
+my_tuple = ('d', 'c', 'e', 'a', 'b')
+my_string = 'python'
+
+#print(my_list, 'original')
+# print(my_list.sort())
+# print(sorted(my_list))
+#print(my_list, 'new')
+
+#print(my_dict, 'original')
+#print(sorted(my_dict.values(), reverse=True))
+#print(my_dict, 'new')
+
+#print(my_list, 'original')
+# print(list(reversed(my_list)))
+#print(my_list, 'new')
+# print(my_list[::1])  # copy
+# print(my_list[::-1])  # reverse copy
+my_llist = [['car', 4, 65], ['dog', 2, 30], ['bee', 1, 24]]
+#print(sorted(my_list, key=abs))
+print(sorted(my_llist, key=lambda item: item[1]))
